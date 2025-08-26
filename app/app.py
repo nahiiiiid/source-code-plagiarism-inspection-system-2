0fb0d5bc84d9feeb2c3ed6134fdfffb66fcb42b5
naahiiid <nahidhasansaikot.com>
2025-08-27 02:47:01 +0600

    Deploy Flask app on Streamlit Cloud using Gunicorn

--- app/app.py
+++ app/app.py
@@ -4,4 +4,5 @@ import os
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')), debug=True)
+    #app.run(host='0.0.0.0', port=int(os.getenv('PORT', '5000')), debug=True)
+    app = create_app()

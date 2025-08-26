b490bd0b13708232800cf0640172bf386379737f
naahiiid <nahidhasansaikot.com>
2025-08-27 02:19:06 +0600

    Add the path to sys.path

--- app/__main__.py
+++ app/__main__.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(__file__))  # Adds current folder to Python path
+from web import create_app
+
+
 from .web import create_app
 app = create_app()
 
